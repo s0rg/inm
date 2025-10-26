563b36a095f21ebd5b714980f767637a8841b4f5
Shevchenko Alexey <al3x.s0rg@gmail.com>
2025-10-26 03:19:15 +0300

    deps up, released as 1.0.0

--- rand.go
+++ rand.go
@@ -13,6 +13,6 @@ func defaultRandom() (r *random) {
 	return &random{src: rand.NewSource(time.Now().UnixNano())}
 }
 
-func (r *random) Rand(max int) (rv int) {
-	return int(r.src.Int63() % int64(max))
+func (r *random) Rand(n int) (rv int) {
+	return int(r.src.Int63() % int64(n))
 }
